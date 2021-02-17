e141513861927dd5c2abd1c73786d6fb0008a6fc
Cho-Eunhak <02069@naver.com>
2021-02-17 21:45:20 +0900

    순수 jdbc 활용

--- hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -1,12 +1,10 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
