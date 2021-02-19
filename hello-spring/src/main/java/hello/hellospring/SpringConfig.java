49b13615e0d5f7f1b99188b8d68c7de878c27936
Cho-Eunhak <02069@naver.com>
2021-02-19 23:46:40 +0900

    스프링 data jpa

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -6,21 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-import javax.swing.*;
-
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+
+
+    private final MemberRepository memberRepository;
 
 
     @Autowired
-    public SpringConfig(EntityManager em){
-        this.em=em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+
+//    private EntityManager em;
+//
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em){
+//        this.em=em;
+//    }
+
+
+
 //    private DataSource dataSource;
 //
 //    @Autowired
@@ -30,13 +40,13 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        //return new JdbcTemplateMemberRepository(dataSource); 
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 
 }
